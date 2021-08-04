f3afa7beea9802ff96f06d1accbddefab4299b5c
Suraj Rimal <surajrimal50@gmail.com>
2021-08-03 19:20:25 -0500

    static file added

--- portfolio-project/portfolio/urls.py
+++ portfolio-project/portfolio/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', jobs.views.suraj, name='suraj'),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
